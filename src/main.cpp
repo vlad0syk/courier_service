bcc16cddc5dc91fe88779e0886de58a202d9bde8
Vlad Sapozhnyk <sapoznikvladislav58@gmail.com>
2025-10-25 15:56:34 +0300

    finished services module

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "auth/Auth.h"
+#include "services/Services.h"
 
 int main() {
     while (true) {
         std::string userLogin = loginUser();
         if (!userLogin.empty()) {
             std::cout << "You are logged in as: " << userLogin << "\n";
-            break;
+            showServiceMenu(userLogin);
         } else {
             std::cout << "Please try again.\n";
         }
